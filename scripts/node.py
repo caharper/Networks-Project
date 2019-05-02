4d2c2d3ba163d4d5d36fc8730acb385d658ff471
caharper <caharper23@gmail.com>
2019-05-02 15:59:14 -0500

    First commit with node

---
+++ scripts/node.py
@@ -0,0 +1,4 @@
+class Node:
+    def __init__(self, id, packetstream):
+        self.id = id
+        self.packetstream = packetstream
---
+++ scripts/sim_driver.py
@@ -0,0 +1,4 @@
+from node import Node
+
+# Create a node 
+n1 = Node(1, 5)
