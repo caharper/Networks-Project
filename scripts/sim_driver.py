fcc0f5e041fa9240fbea9e50824a03c577d5ce28
caharper <caharper23@gmail.com>
2019-05-02 16:12:54 -0500

    made distributions

--- scripts/sim_driver.py
+++ scripts/sim_driver.py
@@ -1,4 +1,20 @@
 from node import Node
+import numpy as np
 
-# Create a node 
+# Create a node
 n1 = Node(1, 5)
+
+
+# Normal Distribution
+mu, sigma = 0, 0.1 # mean and standard deviation
+normal = np.random.normal(mu, sigma, 1000)
+# print(normal)
+
+# Pareto Distribution
+a, m = 3., 2.  # shape and mode
+pareto = (np.random.pareto(a, 1000) + 1) * m
+# print(pareto)
+
+# Uniform/Constant Distribution
+uniform = np.random.uniform(-1,0,1000)
+# print(uniform)
